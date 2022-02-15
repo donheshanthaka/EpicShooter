461c8460e85c825d6de877fee3a55b52d3c270bb
Don Heshanthaka <heshanthaka007@gmail.com>
2022-02-15 14:33:27 +0530

    Bind the star opacity to the pick up widget

--- Source/EpicShooter/Item.h
+++ Source/EpicShooter/Item.h
@@ -6,6 +6,17 @@
 #include "GameFramework/Actor.h"
 #include "Item.generated.h"
 
+UENUM(BlueprintType)
+enum class EItemRarity : uint8 {
+	EIR_Damaged UMETA(DisplayName = "Damaged"),
+	EIR_Common UMETA(DisplayName = "Common"),
+	EIR_Uncommon UMETA(DisplayName = "Uncommon"),
+	EIR_Rare UMETA(DisplayName = "Rare"),
+	EIR_Legendary UMETA(DisplayName = "Legendary"),
+
+	EIR_MAX UMETA(DisplayName = "DefaultMax")
+};
+
 UCLASS()
 class EPICSHOOTER_API AItem : public AActor
 {
@@ -26,6 +37,9 @@ protected:
 	/** Called when end overlapping AreaSphere */
 	UFUNCTION()
 	void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	/** Sets the ActiveStars array of bools based on rarity */
+	void SetActiveStars();
 
 public:	
 	// Called every frame
@@ -55,6 +69,13 @@ private:
 	/** Item count (ammo, etc.) */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
 	int32 ItemCount;
+
+	/** Item rarity - determines number of stars in pickup widget */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	EItemRarity ItemRarity;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	TArray<bool> ActiveStars;
 
 public:
 
