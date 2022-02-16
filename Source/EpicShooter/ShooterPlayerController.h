f54d6057b7f7810235d6ec6f0f525784edf1db7e
Don Heshanthaka <heshanthaka007@gmail.com>
2022-02-16 21:45:42 +0530

    Made the Ammo widget visible on screen through a HUD overlay and created a new Shooter player controller class

---
+++ Source/EpicShooter/ShooterPlayerController.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ShooterPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EPICSHOOTER_API AShooterPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	AShooterPlayerController();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+private:
+
+	/** Reference to the overall HUD Overlay Blueprint Class */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Widgets, meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<class UUserWidget> HUDOverlayClass;
+
+	/** Variable to hold the HUD overlay widget after creating it */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Widgets, meta = (AllowPrivateAccess = "true"))
+	UUserWidget* HUDOverlay;
+};
