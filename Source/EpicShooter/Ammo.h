f839bcbc91ce4a6c92560c23dc2cb71b3f0a0f2f
Don Heshanthaka <heshanthaka007@gmail.com>
2022-02-19 17:46:33 +0530

    Implementation of pickup ammo on character overlap

--- Source/EpicShooter/Ammo.h
+++ Source/EpicShooter/Ammo.h
@@ -27,6 +27,10 @@ protected:
 	/** Override of SetItemProperties so we can set AmmoMesh properties */
 	virtual void SetItemProperties(EItemState State) override;
 
+	/** Called when overlapping AreaSphere */
+	UFUNCTION()
+	void AmmoSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
 private:
 
 	/** Mesh for the ammo pickup */
@@ -40,6 +44,10 @@ private:
 	/** The texture for the ammo icon */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Ammo, meta = (AllowPrivateAccess = "true"))
 	UTexture2D* AmmoIconTexture;
+
+	/** Overlap sphere for picking up the ammo */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Ammo, meta = (AllowPrivateAccess = "true"))
+	class USphereComponent* AmmoCollisionSphere;
 
 
 public:
