9a9d2ab75b46b0a65d46223e48115ed71d15517a
Don Heshanthaka <heshanthaka007@gmail.com>
2022-02-19 13:34:21 +0530

    Creation of ammo class

---
+++ Source/EpicShooter/Ammo.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "Ammo.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EPICSHOOTER_API AAmmo : public AItem
+{
+	GENERATED_BODY()
+	
+};
