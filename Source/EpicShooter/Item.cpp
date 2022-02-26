d5d80177c6888bfce5bc65f0eba2ac9ead4f3121
Don Heshanthaka <heshanthaka007@gmail.com>
2022-02-26 07:42:08 +0530

    Populated the ItemRarityDataTable

--- Source/EpicShooter/Item.cpp
+++ Source/EpicShooter/Item.cpp
@@ -358,6 +358,13 @@ void AItem::OnConstruction(const FTransform& Transform)
 		ItemMesh->SetMaterial(MaterialIndex, DynamicMaterialInstance);
 	}
 	EnableGlowMaterial();
+
+	// Load the data in the item rarity data table
+
+	// Path to the item rarity data table
+	FString RarityTablePath(TEXT("DataTable'/Game/_Game/DataTable/ItemRarityDataTable.ItemRarityDataTable'"));
+	UDataTable* RarityTableObject = Cast<UDataTable>(StaticLoadObject(UDataTable::StaticClass(), nullptr, *RarityTablePath));
+	
 }
 
 void AItem::EnableGlowMaterial()
