7dda850a611b2e1f4e048ff75b3a6a3e1a8902aa
Don Heshanthaka <heshanthaka007@gmail.com>
2022-03-04 12:23:23 +0530

    Creation of AI controller class

---
+++ Source/EpicShooter/EnemyController.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "EnemyController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EPICSHOOTER_API AEnemyController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	AEnemyController();
+	virtual void OnPossess(APawn* InPawn) override;
+
+private:
+	/** Backboard component for this enemy */
+	UPROPERTY(BlueprintReadWrite, Category = "AI Behavior", meta = (AllowPrivateAccess = "true"))
+	class UBlackboardComponent* BlackboardComponent;
+
+	/** Behavior tree component fot this enemy */
+	UPROPERTY(BlueprintReadWrite, Category = "AI Behavior", meta = (AllowPrivateAccess = "true"))
+	class UBehaviorTreeComponent* BehaviorTreeComponent;
+
+public:
+
+	FORCEINLINE UBlackboardComponent* GetBlackboardComponent() const { return BlackboardComponent; }
+
+};
