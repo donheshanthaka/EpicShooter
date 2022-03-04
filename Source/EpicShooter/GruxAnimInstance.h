b828ece11ad307c0b0c63214ea4eae8854fd30a5
Don Heshanthaka <heshanthaka007@gmail.com>
2022-03-05 02:49:55 +0530

    Implementation of enemy run animation

--- Source/EpicShooter/GruxAnimInstance.h
+++ Source/EpicShooter/GruxAnimInstance.h
@@ -14,4 +14,17 @@ class EPICSHOOTER_API UGruxAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
 	
+public:
+
+	UFUNCTION(BlueprintCallable)
+	void UpdateAnimationProperties(float DeltaTime);
+
+private:
+
+	/** Lateral movement speed */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float Speed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class AEnemy* Enemy;
 };
