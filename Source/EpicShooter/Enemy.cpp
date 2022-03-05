0cab15de865e170d0a114845162c1bfcea15f0c0
Don Heshanthaka <heshanthaka007@gmail.com>
2022-03-05 17:58:54 +0530

    Activation and deactivation of weapon collision

--- Source/EpicShooter/Enemy.cpp
+++ Source/EpicShooter/Enemy.cpp
@@ -288,3 +288,18 @@ float AEnemy::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AC
 	return DamageAmount;
 }
 
+void AEnemy::ActivateLeftWeapon() {
+	LeftWeaponCollision->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+}
+
+void AEnemy::DeactivateLeftWeapon() {
+	LeftWeaponCollision->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void AEnemy::ActivateRightWeapon() {
+	RightWeaponCollision->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+}
+
+void AEnemy::DeactivateRightWeapon() {
+	RightWeaponCollision->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
--- Source/EpicShooter/Enemy.h
+++ Source/EpicShooter/Enemy.h
@@ -66,6 +66,19 @@ protected:
 	UFUNCTION()
 	void OnRightWeaponOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
+	// Activate / deactivate collision for weapon boxes 
+	UFUNCTION(BlueprintCallable)
+	void ActivateLeftWeapon();
+
+	UFUNCTION(BlueprintCallable)
+	void DeactivateLeftWeapon();
+
+	UFUNCTION(BlueprintCallable)
+	void ActivateRightWeapon();
+
+	UFUNCTION(BlueprintCallable)
+	void DeactivateRightWeapon();
+
 private:
 
 
